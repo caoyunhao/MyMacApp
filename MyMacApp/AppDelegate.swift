ede7e36fc42cf7eb823ce6ace1d2519c4deccba1
caoyunhao <yunhaocao@qq.com>
2019-05-23 20:32:23 +0800

    Initial Commit

---
+++ MyMacApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyMacApp
+//
+//  Created by Yunhao on 2019/5/23.
+//  Copyright © 2019 Yunhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyMacApp
+//
+//  Created by Yunhao on 2019/5/23.
+//  Copyright © 2019 Yunhao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
